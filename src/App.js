cb4bc949bc984c0bbf4138ce997bdcfd26e9c92d
ralhuang <ralhuang@cs.stonybrook.edu>
2020-03-24 18:10:49 -0400

    start navBar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // LIBRARIES
 import React from "react";
+import './App.css';
 import {
   BrowserRouter as Router,
   Switch,
@@ -11,47 +12,44 @@ import {
 import Login from './components/login/login.js';
 import Home from './components/home/home.js';
 
-// This site has 3 pages, all of which are rendered
-// dynamically in the browser (not server rendered).
-//
-// Although the page does not ever refresh, notice how
-// React Router keeps the URL up to date as you navigate
-// through the site. This preserves the browser history,
-// making sure things like the back button and bookmarks
-// work properly.
-
 export default function BasicExample() {
   return (
     <Router>
       <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-        </ul>
+        <div className="header">
+          <ul className="navBar">
+            <li className="navLink">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="navLink">
+              <Link to="/">Resources</Link>
+            </li>
+            <li className="navLink">
+              <Link to="/">Student Services</Link>
+            </li>
+            <li className="navLink">
+              <Link to="/">Administration</Link>
+            </li>
+            <li className="navLink">
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </div>
 
-        <hr />
-
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
-        <Switch>
-          <Route exact path="/">
-            <HomeComponent />
-          </Route>
-          <Route path="/login">
-            <LoginComponent />
-          </Route>
-        </Switch>
+        <div className="content">
+          <hr />
+          <Switch>
+            <Route exact path="/">
+              <HomeComponent />
+            </Route>
+            <Route path="/login">
+              <LoginComponent />
+            </Route>
+          </Switch>
+        </div>
       </div>
     </Router>
+      
   );
 }
 
