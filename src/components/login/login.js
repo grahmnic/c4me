a67e27fc90ffbade688d7d943f9c323bba118b6b
Nicholas Chen <grahmnic@gmail.com>
2020-03-24 16:07:19 -0400

    Adding react router and login component.

---
+++ src/components/login/login.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return(
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Login;
